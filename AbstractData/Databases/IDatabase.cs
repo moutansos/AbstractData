109fc97ddfb5f85c30b9de87d470831e355743aa
Ben Brougher <moutansos@gmail.com>
2017-01-05 16:57:40 -0800

    create databases folder

---
+++ AbstractData/Databases/IDatabase.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractData
+{
+    public interface IDatabase
+    {
+        dbType getType();
+        bool isMultiTable();
+    }
+
+    public enum dbType
+    {
+        ExcelFile,
+        CSVFile,
+        AccessDB,
+        SQLServerDB,
+        PostgreSqlDB,
+        MariaDB,
+        SQLiteDB,
+    }
+}
