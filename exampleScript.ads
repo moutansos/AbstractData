ec40e9e005262fdad86013e82b57c5884095c067
Ben Brougher <moutansos@gmail.com>
2017-01-23 11:44:00 -0800

    update main example script and remove old second one

--- exampleScript.ads
+++ exampleScript.ads
@@ -1,4 +1,13 @@
 # Example AbstractData Script
 
-ExcelFile excelFile = "C:\ad-test\excel.xlsx"
-SqlServerDb sqlServerDb = "<<Conection String>>"
+Global {parameterIn} = @paramInName
+Global {staticVar} = "Static Variable"
+
+SQLServerDB sqlDb = "<<ConnectionString>>"
+ExcelFile excelFile = "C:\com\file1.xlsx"
+
+tableReference(sqlDb.Table1 => excelFile.Sheet1)
+Local {localVar} = Table1.Field1 + " Test"
+Field2 => A
+{localVar} => B
+move()
