e96f0907f57dbf5f56b408214b7962f77dc141f1
Ben Brougher <moutansos@gmail.com>
2017-02-11 16:07:50 -0800

    Updating example script

--- exampleScript.ads
+++ exampleScript.ads
@@ -1,4 +1,6 @@
 # Example AbstractData Script
+# This is an ideal showcase of what the script can become.
+# Not all of the features in this are implemented
 
 Global {parameterIn} = @paramInName
 Global {staticVar} = "Static Variable"
@@ -6,7 +8,7 @@ Global {staticVar} = "Static Variable"
 SQLServerDB sqlDb = "<<ConnectionString>>"
 ExcelFile excelFile = "C:\com\file1.xlsx"
 
-tableReference(sqlDb.Table1 => excelFile.Sheet1)
+tableReference(sqlDb>Table1 => excelFile>Sheet1)
 Local {localVar} = Table1.Field1 + " Test"
 Field2 => A
 {localVar} => B
